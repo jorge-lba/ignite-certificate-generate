188a6293abab7bf4a2bb4d5defaac26c9fd52e3e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-05-08 11:52:03 -0300

    Add - Criado o template com handlebars

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,31 @@
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars"
+import dayjs from "dayjs"
+
 import { document } from "../utils/dynamodbClient"
 
 interface ICreateCertificate {
   id: string,
   name: string,
   grade: string
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string
+}
+
+const compile = async function (data:ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "template", "certificate.hbs")
+
+  const html = fs.readFileSync(filePath, "utf-8")
+
+  return handlebars.compile(html)(data)
 }
 
 export const handle = async (event) => {
@@ -17,6 +39,19 @@ export const handle = async (event) => {
       grade
     }
   }).promise()
+
+  const medalPath = path.join(process.cwd(), "src", "template", "selo.png")
+  const medal = fs.readFileSync(medalPath, "base64")
+
+  const data:ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal
+  }
+
+  const content = await compile(data)
 
   return {
     statusCode: 201,
