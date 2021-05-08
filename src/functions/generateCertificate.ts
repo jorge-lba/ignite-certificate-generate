171615779386ed19702e49732f182420adfe7588
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-05-08 12:57:56 -0300

    Add - Regra de negocio para caso o usuario ja exista

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -32,14 +32,27 @@ const compile = async function (data:ITemplate) {
 export const handle = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document.put({
+  const response = await document.query({
     TableName: "users_certificates",
-    Item: {
-      id,
-      name,
-      grade
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id" :id
     }
   }).promise()
+
+  const userAlreadyExists = response.Items[0]
+
+  if( userAlreadyExists) {
+    await document.put({
+      TableName: "users_certificates",
+      Item: {
+        id,
+        name,
+        grade
+      }
+    }).promise()
+  }
+
 
   const medalPath = path.join(process.cwd(), "src", "templates", "selo.png")
   const medal = fs.readFileSync(medalPath, "base64")
